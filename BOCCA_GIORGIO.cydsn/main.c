08ff9ecde3d4ff18d5734b364c889c80c7d18951
Giorgio <giorgio1.bocca@mail.polimi.it>
2020-11-08 16:53:26 +0100

    TopDesign fatto e Pin collegati

--- BOCCA_GIORGIO.cydsn/main.c
+++ BOCCA_GIORGIO.cydsn/main.c
@@ -11,11 +11,21 @@
 */
 #include "project.h"
 
+uint8_t eeprom_value;
+uint8_t sampling_freq;
+
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    EEPROM_Start();
+    
+    eeprom_value = EEPROM_ReadByte(0x00);
+    
+    EEPROM_UpdateTemperature();
+    EEPROM_WriteByte(0x64, 0x00); //0x64 = 100 in base decimale
+    eeprom_value = EEPROM_ReadByte(0x00);
 
     for(;;)
     {
